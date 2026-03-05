c8de11b6bf5494051dadb82e52eda47261120783
akshitdeep-hx <akshitdeeprawat@gmail.com>
2026-03-05 11:17:21 +0000

    Updates

--- day32Q2.c
+++ day32Q2.c
@@ -1,45 +1,86 @@
 /*
-Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
-An input string is valid if:
+Implement the MinStack class:
 
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
  
 
 Example 1:
 
-Input: s = "()"
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
 
-Output: true
+Output
+[null,null,null,null,-3,null,0,-2]
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
+typedef struct {
+    int* stack;
+    int* minStack;
+    int top;
+    int capacity;
+} MinStack;
 
-bool isValid(char* s) {
-    int len = strlen(s);
-    char stack[len];
-    int top = -1;
-    
-    for (int i = 0; i < len; i++) {
-        char c = s[i];
-        
-        if (c == '(' || c == '[' || c == '{') {
-            stack[++top] = c;
-        } else {
-            if (top == -1) return false;
-            
-            char last = stack[top--];
-            if ((c == ')' && last != '(') ||
-                (c == ']' && last != '[') ||
-                (c == '}' && last != '{')) {
-                return false;
-            }
-        }
-    }
-    
-    return top == -1;
+
+MinStack* minStackCreate() {
+    MinStack* obj = (MinStack*)malloc(sizeof(MinStack));
+    obj->capacity = 10000;
+    obj->stack = (int*)malloc(obj->capacity * sizeof(int));
+    obj->minStack = (int*)malloc(obj->capacity * sizeof(int));
+    obj->top = -1;
+    return obj;
 }
+
+void minStackPush(MinStack* obj, int val) {
+    obj->stack[++obj->top] = val;
+    if (obj->top == 0) {
+        obj->minStack[obj->top] = val;
+    } else {
+        int currentMin = obj->minStack[obj->top - 1];
+        obj->minStack[obj->top] = val < currentMin ? val : currentMin;
+    }
+}
+
+void minStackPop(MinStack* obj) {
+      if (obj->top >= 0) {
+        obj->top--;
+    }
+}
+
+int minStackTop(MinStack* obj) {
+    return obj->stack[obj->top];
+}
+
+int minStackGetMin(MinStack* obj) {
+    return obj->minStack[obj->top];
+}
+
+void minStackFree(MinStack* obj) {
+      if (obj == NULL) {
+        return;
+    }
+}
+
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * MinStack* obj = minStackCreate();
+ * minStackPush(obj, val);
+ 
+ * minStackPop(obj);
+ 
+ * int param_3 = minStackTop(obj);
+ 
+ * int param_4 = minStackGetMin(obj);
+ 
+ * minStackFree(obj);
+*/
